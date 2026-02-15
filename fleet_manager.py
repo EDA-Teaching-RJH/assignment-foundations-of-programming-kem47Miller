051b977043cd9cfc60417fc39ecae90a9d2b1c3d
kem47 <kem47@kent.ac.uk>
2026-02-15 20:35:11 +0000

    11

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,15 +1,25 @@
 def init_database():
-    names=[ "Jean-Luc Picard", "William T. Riker", "Data", "Worf", "Geordi La Forge" ]
-    ranks=["Captain","Commander","Lieutenant Commander","Lieutenant","Lieutenant Commander"]
-    divisions=["Command", "Command", "Operations", "Security","Operations"]
+    names=[
+         "Jean-Luc Picard", 
+         "William T. Riker",
+           "Data", "Worf", 
+           "Geordi La Forge"
+           ]# easier to read
+    ranks=[
+        "Captain","Commander","Lieutenant Commander",
+        "Lieutenant","Lieutenant Commander"
+        ]
+    divisions=[
+        "Command", "Command", "Operations", "Security",
+               "Operations"]
     ids=["00","01","02","03","04"]
-    return names, ranks,divisions,ids
+    return names, ranks,divisions, ids
 
 def display_menu():
    student_login =input("State your full name: ")
    print(f"Hello {student_login},Loading Option menu now...")
    while True:#as long as True print this forever
-        print("----OPTION MENU----")
+        print("**YOUR OPTION MENU*****")
         print("1. Add crew member")
         print("2. Remove crew member")
         print("3. Update crew member rank")
@@ -21,13 +31,13 @@ def display_menu():
         print("9. Exit")
         
         try:
-            choice = int(input(f"State your option (1-9), {student_login}: "))
-            if 1 <= choice <= 9:# if the input is 1 to 9
-                return choice#exit loop
+            opt_picked =int(input(f"State your option (1-9),{student_login}: "))
+            if 1 <= opt_picked <= 9:# if the input is 1 to 9
+                return opt_picked#exit loop
             else:# if input not 1 to 9
-                print("Enter a number 1 to  9.")#print this
+                print("Mhmm...that's not right. Try picking a # 1 to  9.")#print this
         except ValueError:#if input not a number
-            print("Enter a number 1 to 9.")#print this
+            print("No. Pick a NUMBER from ONE to TEN.")#print this
 
 
 
---
+++ fleet_manager1.py
@@ -0,0 +1,62 @@
+#init_database(): Returns 4 lists pre-populated with at least 5 Star Trek characters and their data.
+def init_database():
+    names:[""]
+    ranks:
+    divisions:
+    ids:
+    return 
+
+
+#display_menu(): Queries users full name, Prints the options and current student logged in and returns the user's choice.
+
+#add_member(names, ranks, divs, ids):
+
+#Validates ID is unique.
+
+#Validates Rank is a valid TNG rank.
+
+#Appends data to all 4 lists.
+
+#remove_member(names, ranks, divs, ids):
+
+#Asks for an ID.
+
+#Finds the index.
+
+#Removes the entry from all 4 lists to keep them in sync.
+
+#update_rank(names, ranks, ids):
+
+#Finds a member by ID.
+
+#Updates their rank string.
+
+#display_roster(names, ranks, divs, ids):
+
+#Iterates through the lists using range(len(names)).
+
+#Prints a formatted table of all crew.
+
+#search_crew(names, ranks, divs, ids):
+
+#Asks for a search term.
+
+#Prints any crew member whose name contains that term.
+
+#filter_by_division(names, divs):
+
+#Asks user for "Command", "Operations", or "Sciences".
+
+#Prints only members in that division using match or if .
+
+#calculate_payroll(ranks):
+
+#Iterates through the ranks list.
+
+#Assigns a credit value to ranks (e.g., Captain = 1000, Ensign = 200).
+
+#Returns the total cost of the crew.
+
+#count_officers(ranks):
+
+#Counts how many "Captain" and "Commander" ranks exist and returns the integer.
